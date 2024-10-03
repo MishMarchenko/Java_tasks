82df0c7dbdffdc677731396a2bf81ad4bb2e6cf0
MishMarchenko <mishmar4enko@gmail.com>
2024-10-03 13:21:02 +0300

    dffdg

--- src/TasksPart2.java
+++ src/TasksPart2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TasksPart2 {
@@ -14,28 +15,32 @@ public class TasksPart2 {
                 break;
             case 2:
                 System.out.println("Enter a number and I will return you number of digits in a number");
-                System.out.println(sumOfDigitsInNumber(scanner.nextInt()));
+                System.out.println(countDigitsInNumber(scanner.nextInt()));
                 break;
             case 3:
                 System.out.println("Enter number and I will check if it is a palindrome");
                 scanner.nextLine();
                 palindromeChecks(scanner.nextLine());
                 break;
-//            case 4:
-//                System.out.println("tbd");
-//                break;
+            case 4:
+                System.out.println("Enter five numbers and I will get you AVG value form them");
+                getAvgValue(scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+                break;
 //            case 5:
 //                System.out.println("tbd");
 //                break;
-//            case 6:
-//                System.out.println("tbd");
-//                break;
-//            case 7:
-//                System.out.println("tbd");
-//                break;
-//            case 8:
-//                System.out.println("tbd");
-//                break;
+            case 6:
+                System.out.println("Enter the number and I will return you factorial of this number");
+                System.out.println(factorial(scanner.nextInt()));
+                break;
+            case 7:
+                System.out.println("Enter 10 numbers and I will sorted them");
+                sortingByBubbleSort(scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+                break;
+            case 8:
+                System.out.println("I made a random number from 1 till 100, try to guess it!");
+                guessNumber(scanner.nextInt(), generateNumber());
+                break;
 //            case 9:
 //                System.out.println("tbd");
 //                break;
@@ -48,19 +53,18 @@ public class TasksPart2 {
         }
     }
     public static void sumOfNumbers(int a, int b){
-        Scanner scanner = new Scanner(System.in);
         if (a > b){
-            System.out.println("First number is less then second. Enter correct numbers.");
-            sumOfNumbers(scanner.nextInt(), scanner.nextInt());
-        } else {
-            int sum = 0;
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        int sum = 0;
             for (int i = a; i <= b; i++) {
                 sum = sum + i;
             }
             System.out.println(sum);
-        }
     }
-    public static int sumOfDigitsInNumber(int num){
+    public static int countDigitsInNumber(int num){
         return String.valueOf(num).length();
     }
     public static void palindromeChecks(String st){
@@ -87,8 +91,59 @@ public class TasksPart2 {
         if (max2 != Integer.MIN_VALUE){
             System.out.println("Second max value: " + max2);
         } else {
-            System.out.println("There is no second max value");
+            System.out.println("All numbers is equal");
         }
         System.out.println("Max value: " + max);
+    }
+    public static void sortingByBubbleSort(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j){
+        int [] array = new int[] {a, b, c, d, e, f, g, h, i, j};
+        boolean swapped;
+        for (int l = 0; l < array.length - 1; l++) {
+            swapped = false;
+            for (int m = 0; m < array.length - 1 - l; m++) {
+                if (array[m] > array[m + 1]) {
+                    int tmp = array[m];
+                    array[m] = array[m + 1];
+                    array[m + 1] = tmp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    public static void getAvgValue(int a, int b, int c, int d, int e){
+        int [] array = new int[]{a, b, c, d, e};
+        double sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        double avg = sum / array.length;
+        System.out.println("Avg value is: " + avg);
+    }
+    public static int factorial(int a){
+        if(a == 1 || a == 0){
+            return 1;
+        }
+        return a * factorial(a - 1);
+    }
+    public static void guessNumber(int a, int b){
+        Scanner scanner = new Scanner(System.in);
+        if(a == b){
+            System.out.println("You did it!");
+        } else if (a < b && a > 0) {
+            System.out.println("My number is bigger, try again");
+            guessNumber(scanner.nextInt(), b);
+        } else if(a > b && a < 101){
+            System.out.println("My number is less, try again");
+            guessNumber(scanner.nextInt(), b);
+        } else {
+            System.out.println("I have just made the number from 1 till 100. Enter correct number");
+            guessNumber(scanner.nextInt(), b);
+        }
+    }
+    public static int generateNumber(){
+        int num = (int) (Math.random() * 100) + 1;
+        return num;
     }
 }
